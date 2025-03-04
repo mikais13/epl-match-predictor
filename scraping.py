346067160cd890754e9e4fd7140268d7d0f01895
mikais13 <mikai.somerville@gmail.com>
2025-03-04 23:50:21 +1300

    Update web scraping logic to include additional data and improve performance

--- scraping.py
+++ scraping.py
@@ -3,12 +3,13 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import time
 
-years = list(range(2024, 2020, -1))
+years = list(range(2024, 2022, -1))
 all_matches = []
 standings_url = "https://fbref.com/en/comps/9/Premier-League-Stats"
 for year in years:
     data = requests.get(standings_url)
-    soup = BeautifulSoup(data.text)
+    print(data, data.headers)
+    soup = BeautifulSoup(data.text, features="lxml")
     standings_table = soup.select('table.stats_table')[0]
     
     links = [l.get('href') for l in standings_table.find_all('a')]
@@ -20,15 +21,18 @@ for year in years:
     
     for team_url in team_urls:
         team_name = team_url.split("/")[-1].replace("-Stats", "").replace("-", " ")
+        print(team_name, year)
         
-        data = requests.get(team_url)
-        matches = pd.read_html(data.text, match="Scores & Fixtures")[0]
-        
-        soup = BeautifulSoup(data.text, features="lxml")
-        links = soup.find_all('a')
-        links = [l.get("href") for l in links]
-        links = [l for l in links if l and 'all_comps/shooting/' in l]
         try:
+            data = requests.get(team_url)
+            print(data)
+            matches = pd.read_html(data.text, match="Scores & Fixtures")[0]
+            
+            soup = BeautifulSoup(data.text, features="lxml")
+            links = soup.find_all('a')
+            links = [l.get("href") for l in links]
+            links = [l for l in links if l and 'all_comps/shooting/' in l]
+        
             data = requests.get(f"https://fbref.com{links[0]}")
             shooting = pd.read_html(data.text, match="Shooting")[0]
             shooting.columns = shooting.columns.droplevel()        
@@ -40,4 +44,8 @@ for year in years:
         team_data["Season"] = year
         team_data["Team"] = team_name
         all_matches.append(team_data)
-        time.sleep(1)
+        time.sleep(10)
+
+match_df = pd.concat(all_matches)
+match_df.columns = [c.lower() for c in match_df.columns]
+match_df.to_csv("matches.csv")
