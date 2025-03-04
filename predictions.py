d7fc6f81808b7e0e17c2bcae2d58288c04803a49
mikais13 <mikai.somerville@gmail.com>
2025-03-04 23:56:34 +1300

    Enhance prediction model by updating data source and adding formation feature

--- predictions.py
+++ predictions.py
@@ -2,13 +2,14 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import precision_score
 
-matches = pd.read_csv("matches_supplied.csv", index_col=0)
+matches = pd.read_csv("matches.csv", index_col=0)
 
 matches["date"] = pd.to_datetime(matches["date"])
 matches["venue_code"] = matches["venue"].astype("category").cat.codes
 matches["opp_code"] = matches["opponent"].astype("category").cat.codes
 matches["hour"] = matches["time"].str.replace(":.+", "", regex=True).astype("int")
 matches["day_code"] = matches["date"].dt.day_of_week
+# matches["formation_code"] = matches["formation"].astype("category").cat.codes
 matches["target"] = (matches["result"] == "W").astype("int")
 
 grouped_matches = matches.groupby("team")
@@ -20,16 +21,16 @@ def rolling_averages(group, cols, new_cols):
     group = group.dropna(subset=new_cols)
     return group
 
-cols = ["gf", "ga", "sh", "sot", "dist", "fk", "pk", "pkatt"]
+cols = ["gf", "ga", "sh", "sot", "dist", "fk", "pk", "pkatt", "xg"]
 new_cols = [f"{c}_rolling" for c in cols]
 matches_rolling = matches.groupby("team").apply(lambda x: rolling_averages(x, cols, new_cols))
 matches_rolling = matches_rolling.droplevel("team")
 matches_rolling.index = range(matches_rolling.shape[0])
 
 def make_predictions(data, predictors):
-    rf = RandomForestClassifier(n_estimators=50, min_samples_split=10, random_state=1)
-    train = data[data["date"] < "2022-01-01"]
-    test = data[data["date"] >= '2022-01-01']
+    rf = RandomForestClassifier(n_estimators=5000, min_samples_split=10, random_state=0)
+    train = data[data["date"] < "2024-06-06"]
+    test = data[data["date"] >= '2024-06-06']
     rf.fit(train[predictors], train["target"])
     preds = rf.predict(test[predictors])
     combined = pd.DataFrame(dict(actual=test["target"], predicted=preds), index=test.index)
@@ -49,10 +50,13 @@ map_values = {
     "Newcastle United": "Newcastle Utd",
     "Tottenham Hotspur": "Totteham",
     "West Ham United": "West Ham",
-    "Wolverhampton Wanderers": "Wolves"
+    "Wolverhampton Wanderers": "Wolves",
+    "Nottingham Forest": "Nott'ham Forest",
+    "Sheffield United": "Sheffield Utd"
 }
 mapping = MissingDict(**map_values)
 combined["new_team"] = combined["team"].map(mapping)
 merged = combined.merge(combined, left_on=["date", "new_team"], right_on=["date", "opponent"])
 result = merged[(merged["predicted_x"] == 1) & (merged["predicted_y"] == 0)]["actual_x"].value_counts()
+print(result)
 print(result[1]/(result[0]+result[1]))
